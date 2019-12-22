b2adebdf85a8f2a12d9a9e5755f0f4861e9d0e08
Dylan-TerMolen <dptermolen@gmail.com>
2019-12-21 19:24:41 -0600

    Updates

--- precache-manifest.5fd7e5ed7b9661075081eadf8a3d2a5d.js
+++ precache-manifest.5fd7e5ed7b9661075081eadf8a3d2a5d.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f6dcce843f8c3b3e6fe7b5b523abf05c",
+    "revision": "88cca55f5478cd19ea6049d4d244d04f",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "b23b952f77bb5f4f6773",
+    "revision": "17246b6381b1e9cd49b0",
     "url": "/portfolio/static/css/main.8360ab46.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.c8cfbe64.chunk.js.LICENSE"
   },
   {
-    "revision": "b23b952f77bb5f4f6773",
-    "url": "/portfolio/static/js/main.490a8f2d.chunk.js"
+    "revision": "17246b6381b1e9cd49b0",
+    "url": "/portfolio/static/js/main.201fc958.chunk.js"
   },
   {
     "revision": "8575fab3f3395c90d845",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a6d3c8f750ff133c686032f5f2534a58.js"
+  "/portfolio/precache-manifest.5fd7e5ed7b9661075081eadf8a3d2a5d.js"
 );
 
 self.addEventListener('message', (event) => {
