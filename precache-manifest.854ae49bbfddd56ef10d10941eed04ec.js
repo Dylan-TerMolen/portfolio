69d0d7089edd32131885acd06721963b8af4d23c
Dylan-TerMolen <dptermolen@gmail.com>
2019-12-24 01:13:45 -0600

    Updates

--- precache-manifest.854ae49bbfddd56ef10d10941eed04ec.js
+++ precache-manifest.854ae49bbfddd56ef10d10941eed04ec.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "660c1b3665febcd53cf789af299c7363",
+    "revision": "a4fc1c7e7fe6a7558950ec24f61d469b",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "457fc6f5ce0ecbf628b8",
+    "revision": "9520123d9a2514089908",
     "url": "/portfolio/static/css/main.8360ab46.chunk.css"
   },
   {
-    "revision": "428f705f607786e18118",
-    "url": "/portfolio/static/js/2.ce2bb52f.chunk.js"
+    "revision": "3297bb3f89a2e1f42ab8",
+    "url": "/portfolio/static/js/2.fd9f073a.chunk.js"
   },
   {
     "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
-    "url": "/portfolio/static/js/2.ce2bb52f.chunk.js.LICENSE"
+    "url": "/portfolio/static/js/2.fd9f073a.chunk.js.LICENSE"
   },
   {
-    "revision": "457fc6f5ce0ecbf628b8",
-    "url": "/portfolio/static/js/main.e3b299e5.chunk.js"
+    "revision": "9520123d9a2514089908",
+    "url": "/portfolio/static/js/main.c9a88a06.chunk.js"
   },
   {
     "revision": "8575fab3f3395c90d845",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0004a9cdcfec5ccbda2e0378d8196684.js"
+  "/portfolio/precache-manifest.854ae49bbfddd56ef10d10941eed04ec.js"
 );
 
 self.addEventListener('message', (event) => {
